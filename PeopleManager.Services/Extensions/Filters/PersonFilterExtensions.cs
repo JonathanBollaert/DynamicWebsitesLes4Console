e0dc63c9b18182b964e625fec26cc89024b81857
JonathanBollaert <159426039+JonathanBollaert@users.noreply.github.com>
2024-10-16 12:50:18 +0200

    Initial commit

---
+++ PeopleManager.Services/Extensions/Filters/PersonFilterExtensions.cs
@@ -0,0 +1,34 @@
+﻿using PeopleManager.Dto.Filters;
+using PeopleManager.Dto.Results;
+using PeopleManager.Model;
+
+namespace PeopleManager.Services.Extensions.Filters
+{
+    public static class PersonFilterExtensions
+    {
+        public static IQueryable<PersonResult> ApplyFilter(this IQueryable<PersonResult> query, PersonFilter? filter)
+        {
+            if (filter is null)
+            {
+                return query;
+            }
+
+            if (filter.OrganizationId.HasValue)
+            {
+                query = query.Where(p => p.OrganizationId == filter.OrganizationId);
+            }
+
+            if (!string.IsNullOrWhiteSpace(filter.Search))
+            {
+                query = query.Where(p =>
+                    p.FirstName.Contains(filter.Search)
+                    || p.LastName.Contains(filter.Search)
+                    || (p.Email != null && p.Email.Contains(filter.Search))
+                    || (p.OrganizationName != null && p.OrganizationName.Contains(filter.Search))
+                );
+            }
+
+            return query;
+        }
+    }
+}
---
+++ PeopleManager.Ui.ConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PeopleManager.Dto.Filters;
+using PeopleManager.Sdk;
+using PeopleManager.Sdk.Extensions;
+using PeopleManager.Ui.ConsoleApp.Settings;
+using Vives.Services.Model;
+
+var configurationBuilder = new ConfigurationBuilder();
+var services = new ServiceCollection();
+
+configurationBuilder.SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+var configuration = configurationBuilder.Build();
+
+var apiSettings = new ApiSettings();
+configuration.GetSection(nameof(ApiSettings)).Bind(apiSettings);
+services.AddApi(apiSettings.BaseUrl);
+
+var serviceProvider = services.BuildServiceProvider();
+
+//Show Organizations
+var organizationService = serviceProvider.GetRequiredService<OrganizationSdk>();
+
+var paging = new Paging { Offset = 0, Limit = 1000 };
+var organizations = await organizationService.Find(paging);
+
+foreach (var organization in organizations)
+{
+    Console.WriteLine($"[{organization.Id}] {organization.Name} ({organization.NumberOfMembers} members)");
+}
+
+//Show People
+var personService = serviceProvider.GetRequiredService<PersonSdk>();
+
+var people = await personService.Find(paging, null);
+
+foreach (var person in people)
+{
+    Console.WriteLine($"[{person.Id}] {person.FirstName} {person.LastName} ({person.OrganizationName})");
+}
+
+Console.ReadLine();
